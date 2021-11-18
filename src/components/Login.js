640ab40b215165986734dcfffe9b9292693c2d04
Augusto Seabra <augusto.seabra00@gmail.com>
2021-11-17 21:06:05 -0300

    Corrigindo CSS e removendo métodos não utilizados

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react"
-import axios from "axios"
 import Input from "./Input"
 import { Link } from 'react-router-dom';
 
@@ -10,28 +9,9 @@ const formData = {
 
 const Login = () => {
   const [login, setLogin] = useState(formData)
-  const [isLoading, setIsLoading] = useState(false)
 
   const setInput = newValue => {
     setLogin(login => ({...login, ...newValue}))
-  }
-
-  const postLogin = async login => {
-    try {
-      setIsLoading(true)
-      const response = await axios.post(`http://xxxx/api/login`, login)
-      if(!response || typeof response != "object") throw new Error("Retorno da API incorreto")
-      setIsLoading(false)
-    }catch(e) {
-      console.error("Erro ao enviar os dados de login")
-      console.error(e)
-      setIsLoading(false)
-    }
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    postLogin(login)
   }
 
   return (
@@ -41,7 +21,7 @@ const Login = () => {
       </h1>
       <div className="container-sm p-5 bg-dark rounded-1 text-light">
         <h2 className="mb-5"> Login </h2>
-        <form onSubmit={handleSubmit}>
+        <form>
           <div className="row">
             <div className="form-group col-md-6">
               <Input
@@ -53,6 +33,7 @@ const Login = () => {
             <div className="form-group col-md-6">
               <Input
                 name="password"
+                type="password"
                 onChange={e => setInput({ password: e.target.value })}
                 label="Senha"
               />
@@ -64,9 +45,7 @@ const Login = () => {
           </div>
           <div className="d-flex">
             <Link to="/register" className="btn bg-green flex-grow-1 me-4">Cadastre-se</Link>
-            <Link to="/home" className="btn flex-grow-1 bg-dark-green text-light">
-              {isLoading ? "Carregando..." : "Acessar"}
-            </Link>
+            <Link to="/home" className="btn flex-grow-1 bg-dark-green text-light">Acessar</Link>
           </div>
         </form>
       </div>
