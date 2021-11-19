4076f54a5c48c9838074b167d9c8a0d2ed22de42
Matheus Xavier <matheuscx@outlook.com>
2021-11-18 22:39:53 -0300

    Adição de regras de code style e correções na identação

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import Login from "./views/Login"
-import Cadastro from "./views/Cadastro"
-import Home from "./views/Home"
-import QuemSomos from "./views/QuemSomos"
-import Admin from "./views/Admin"
+import Login from "./views/Login";
+import Cadastro from "./views/Cadastro";
+import Home from "./views/Home";
+import QuemSomos from "./views/QuemSomos";
+import Admin from "./views/Admin";
 
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <main className="App">
       <Routes>
-        <Route path='/' element={<Login />} exact />
-        <Route path='/register' element={<Cadastro />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/about' element={<QuemSomos />} />
-        <Route path='/admin' element={<Admin />} />
-        <Route path='*' element={<Login />} />
+        <Route path="/" element={<Login />} exact />
+        <Route path="/register" element={<Cadastro />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<QuemSomos />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<Login />} />
       </Routes>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = ({ admin }) => {
-  return(
-    <nav className={`navbar w-100 bg-dark text-light ${admin ? 'p-1' : 'p-3'}`}>
+  return (
+    <nav className={`navbar w-100 bg-dark text-light ${admin ? "p-1" : "p-3"}`}>
       <ul className="m-0 p-0 mx-3 navbar-nav flex-row justify-content-between align-items-center w-100">
         <Link className="navbar-brand text-light" to="/">
-          <img src="./images/mooc_logo_branco.png" className="w-150px" alt="Logo Mooc Branco" />
+          <img
+            src="./images/mooc_logo_branco.png"
+            className="w-150px"
+            alt="Logo Mooc Branco"
+          />
         </Link>
-        <Link className="nav-item d-flex justify-content-center align-items-center text-light text-decoration-none" to="/admin">
+        <Link
+          className="nav-item d-flex justify-content-center align-items-center text-light text-decoration-none"
+          to="/admin"
+        >
           Administrar
         </Link>
-        <Link className="nav-item d-flex justify-content-center align-items-center text-light text-decoration-none" to="/about">
+        <Link
+          className="nav-item d-flex justify-content-center align-items-center text-light text-decoration-none"
+          to="/about"
+        >
           Quem Somos
         </Link>
-        <Link className="nav-item d-flex justify-content-center align-items-center text-light text-decoration-none" to="/register">
+        <Link
+          className="nav-item d-flex justify-content-center align-items-center text-light text-decoration-none"
+          to="/register"
+        >
           Quero me Cadastrar
         </Link>
-        { !admin &&
+        {!admin && (
           <>
             <Link className="nav-item btn btn-outline-green" to="/">
               Acessar minha conta
             </Link>
           </>
-        }
+        )}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
